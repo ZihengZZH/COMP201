799e7624c4ee0b94ad36106be974ac0f025f8429
Ziheng ZHANG <zihengzhang1025@gmail.com>
2017-09-29 11:34:28 +0100

    Add files via upload

--- ConsoleGame_Dice/player.cpp
+++ ConsoleGame_Dice/player.cpp
@@ -1,28 +1,60 @@
 #include "player.h"
 
-class player {
+player::player(std::string input_name, int input_id) {
+    name = input_name;
+    id = input_id;
+    victory = false;
+}
 
-	player::player(std::string input_name) {
+player::~player(void) {
 
 
+}
+
+void player::main(void){
+    while (!victory){
+        run();
+    }
+
+    std::cout << "Congratulations\n";
+
+}
+
+
+
+void player::run(void) {
+    int input;
+    while (true) {
+        std::cout << "Your current score " << get_sum(points) << "\nDo you want to hold the current score?\n";
+        std::cout << "1 to hold; others to cancel\n";
+        std::cin >> input;
+        if (input == 1){
+            std::cout << "Next turn\n";
+            break;
+        }
+        std::cout << "Dice Rolling\n";
+        int dice = rand()%6+1;
+        Sleep(1000);
+        std::cout << "Dice number " << dice << std::endl;
+        if (dice == 1){
+            std::cout << "Next turn\n";
+            break;
+        }else{
+            points.push_back(dice);
+        }
+
+        if (get_sum(points) >= 100){
+            victory = true;
+            std::cout << "You win\n";
+            break;
+        }
+    }
+}
+
+int player::get_sum(std::vector<int>& nums) {
+	int sum = 0;
+	for (int num : nums) {
+		sum += num;
 	}
-
-	player::~player(void) {
-
-
-	}
-
-	void player::run(void) {
-
-
-	}
-
-	int player::get_sum(std::vector<int>& nums) {
-		int sum = 0;
-		for (int num : nums) {
-			sum += num;
-		}
-		return sum;
-	}
-
-};
+	return sum;
+}
--- ConsoleGame_Dice/player.h
+++ ConsoleGame_Dice/player.h
@@ -1,18 +1,21 @@
 #pragma once
 #include <iostream>
 #include <vector>
+#include <stdlib.h>
+#include <windows.h>
 
 class player {
 	
 private:
 	std::string name;
 	int id;
-	bool choice;
+    bool victory;
 	std::vector<int> points;
 
 public:
-	player(std::string input_name);
-	~player(void);
+	player(std::string input_name, int input_id);
+    ~player(void);
+    void main(void);
 	void run(void);
 	int get_sum(std::vector<int>& nums);
 
