c117f529265252f573a41effc5fa26a2ee35b60d
Ziheng ZHANG <zihengzhang1025@gmail.com>
2017-10-03 09:13:21 +0100

    Update main.cpp

--- ConsoleGame_Dice/main.cpp
+++ ConsoleGame_Dice/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include "player.h"
 
 int main() {
-
+	const vector<int> ids = {1,2,3,4,5,6,7,8,9,10};
+	int pt = 0;
+	
+	player A("ZZH",ids[pt]);
+	player B("SQ",ids[pt++]);
+	
+	A.main();
+	B.main();
 
 	return 0;
 }
