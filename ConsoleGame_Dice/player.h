08d7b4778713d9a5ebc11639025b1a699167024a
Ziheng ZHANG <zihengzhang1025@gmail.com>
2017-10-12 15:32:49 +0100

    Add files via upload

--- ConsoleGame_Dice/player.h
+++ ConsoleGame_Dice/player.h
@@ -9,96 +9,97 @@
 using namespace std;
 
 class player {
-    
+
 public:
-    int id;
-    std::string name;
-    vector<int> log;
-    vector<int> points;
-    
+	int id;
+	std::string name;
+	vector<int> log;
+	vector<int> points;
+
 public:
-    bool current;
-    bool victory;
-    
+	bool current;
+	bool victory;
+
 public:
-    player(string input_name, int input_id);
-    ~player(void);
-    void run(void);
-    
-    void show_list(vector<int>& nums);
-    int get_sum(vector<int>& nums);
-    
-    
+	player(string input_name, int input_id);
+	~player(void);
+	void run(void);
+
+	void show_list(vector<int>& nums);
+	int get_sum(vector<int>& nums);
+
+
 };
 
-class autoplayer : public player{
-    
+
+
+class autoplayer : public player {
+
 private:
-    int gain;
-    bool always;
-    vector<int> cur;
+	int gain;
+	bool always;
+	vector<int> cur;
 
 public:
-    
-    autoplayer(string input_name, int input_id, bool input_always) : player(input_name, input_id) {
-        always = input_always;
-        if (!always) {
-            cout << "Please enter gain for each turn ";
-            cin >> gain;
-            cout << endl;
-        }
-    };
-    
-    ~autoplayer(void){};
-    
-    bool enough_gain(vector<int>& cur){
-        int sum = 0;
-        for (auto i : cur){
-            sum += i;
-        }
-        if (sum >= gain){
-            return true;
-        }else{
-            return false;
-        }
-    };
-    
-    
-    void auto_run(void){
-        
-        int dice;
-        while (true){
-            current = {};
-            cout << name << " your current score is _" << get_sum(points) << "_\n";
-            dice = rand() % 6 + 1;
-            log.push_back(dice);
-            cur.push_back(dice);
-            cout << " Dice number _" << dice << "_" << endl;
-            if (dice == 1){
-                cout << "Next turn\n";
-                break;
-            }
-            else {
-                cout << "Keep rolling\n";
-                points.push_back(dice);
-            }
-            
-            if (!always){
-                if(enough_gain(cur)){
-                    cout << "Next turn\n";
-                    break;
-                }
-            }
-            
-            if (get_sum(points) >= 100) {
-                victory = true;
-                cout << "Congrats " << name << "! You win\n";
-                show_list(log);
-                break;
-            }
-        }
-        
-    };
-    
-    
+
+	autoplayer(string input_name, int input_id, bool input_always) : player(input_name, input_id) {
+		always = input_always;
+		if (!always) {
+			cout << "Please enter gain for each turn ";
+			cin >> gain;
+			cout << endl;
+		}
+	};
+
+	~autoplayer(void) {};
+
+	bool enough_gain(vector<int>& cur) {
+		int sum = 0;
+		for (auto i : cur) {
+			sum += i;
+		}
+		if (sum >= gain) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	};
+
+
+	void auto_run(void) {
+		srand(time(0));
+		int dice;
+		while (true) {
+			cur = {};
+			cout << name << " your current score is _" << get_sum(points) << "_\n";
+			dice = rand() % 6 + 1;
+			log.push_back(dice);
+			cur.push_back(dice);
+			Sleep(250);
+			cout << "Dice number _" << dice << "_" << endl;
+			if (dice == 1) {
+				cout << "Next turn\n";
+				break;
+			}
+			else {
+				cout << "Keep rolling\n";
+				points.push_back(dice);
+			}
+
+			if (!always && enough_gain(cur)) {
+				cout << "Next turn\n";
+				break;
+			} 
+			else if (get_sum(points) >= 100) {
+				victory = true;
+				cout << "Congrats " << name << "! You win\n";
+				show_list(log);
+				break;
+			}
+		}
+
+	};
+
+
 };
